422e051e887831a2b48e6bfc51b1ab65bce24598
Ksenia Tuzova <102681427+Ksenia-Tuzova2@users.noreply.github.com>
2023-01-28 04:13:10 +0300

    check commit for deploy with change app.tsx errors

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -1,91 +1,17 @@
-// import React from 'react'
-// import s from './App.module.css'
-
-// import Pages from '../../../p2-homeworks/h5-rrd-v6/Pages';
-
-// function App() {
-//     // debugger
-//     return (
-//         <div className={s.App}>
-         
-//             <Pages/>
-
-//         </div>
-//     )
-// }
-
-// export default App
-import {createStore} from 'redux'
-import ReactDOM from 'react-dom'
-import {Provider, useSelector, useDispatch} from 'react-redux'
 import React from 'react'
+import s from './App.module.css'
 
-const students = {
-    students: [
-        {id: 1, name: 'Bob'},
-        {id: 2, name: 'Alex'},
-        {id: 3, name: 'Donald'},
-        {id: 4, name: 'Ann'},
-    ]
-}
-type RemoveStudentAT = {
-    type: "REMOVE-STUDENT"
-    id: number
-}
-const RemoveStudentAC = (id: number): RemoveStudentAT => ({
-    type: "REMOVE-STUDENT",
-    id
-})
+import Pages from '../../../p2-homeworks/h5-rrd-v6/Pages';
 
-const studentsReducer = (state = students, action: RemoveStudentAT) => {
-    switch (action.type) {
-        case "REMOVE-STUDENT":
-            return {
-                ...state,
-                students: state.students.filter(s => s.id !== action.id)
-            }
-    }
-    return state
-}
-
-const store = createStore(studentsReducer)
-type RootStateType = ReturnType<typeof studentsReducer>
-
-
-const StudentList = () => {
-    const listItemStyles = {
-        width: "100px",
-        borderBottom: "1px solid gray",
-        cursor: "pointer",
-    }
-    const students = useSelector((state: RootStateType) => state.students)
-    const dispatch = useDispatch()
-    const studentsList = students.map(s => {
-        const removeStudent = () => {
-            dispatch(RemoveStudentAC(s.id))
-        }
-        return (
-            <li key={s.id}
-                style={listItemStyles}
-                onClick={removeStudent}>
-                {s.name}
-            </li>)
-    })
+function App() {
+    // debugger
     return (
-        <ol>
-            {studentsList}
-        </ol>
+        <div className={s.App}>
+         
+            <Pages/>
 
+        </div>
     )
 }
 
-
-ReactDOM.render(<div>
-        <Provider store={store}>
-            <StudentList/>
-        </Provider>
-    </div>,
-    document.getElementById('root')
-)
-
-//check for deploy
+export default App
